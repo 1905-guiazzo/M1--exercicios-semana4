d61d8277a31555f375ff7cc7de2358ef29d16da7
1905-guiazzo <72606281+1905-guiazzo@users.noreply.github.com>
2020-12-16 11:06:42 -0300

    .,

--- exercicio10/auto.js
+++ exercicio10/auto.js
@@ -4,4 +4,25 @@ somente resolver a promise caso o resultado seja par retornando o
 valor da soma e rejeitando a promise caso ímpar, 
 retornando uma mensagem explicativa */
 
+const numeroPar = (numero1, numero2)=>{
+    const soma = numero1 + numero2
+    const promise = new Promise((resolve, reject) => {
+        if (soma % 2 === 0){
+           
+            setTimeout(() => resolve(soma + " É par!"), 2000);
+
+        } else {
+            
+            setTimeout(() => reject ("Erro! o número é impar."), 3000);
+        }
+    });
+    return promise;
+};
+numeroPar(4, 3)
+.then((resposta)=>{
+    console.log(resposta);
+})
+.catch((erro)=>console.log(erro))
     
+
+ 
